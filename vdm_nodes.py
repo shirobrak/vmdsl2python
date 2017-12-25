53a8b285dea1ff7b9b077900f5cf67ef00b4d318
Hironari Iwata <hiro_wrty1028_java@yahoo.co.jp>
2017-12-25 11:55:56 +0900

    括弧式, let式 クラス実装

--- vdm_nodes.py
+++ vdm_nodes.py
@@ -35,7 +35,36 @@ class Expression(VdmslNode):
         self.lineno = lineno
         self.lexpos = lexpos
 
+class BracketExpression(VdmslNode):
+    """ 括弧式 """
+    _field = ('body',)
 
+    def __init__(self, body, lineno, lexpos):
+        self.body = body
+        self.lineno = lineno
+        self.lexpos = lexpos
+
+class LetExpression(VdmslNode):
+    """ let式 """
+    _field = ('local_definition', 'body',)
+
+    def __init__(self, local_definition, body, lineno, lexpos):
+        self.local_definition = local_definition
+        self.body = body
+        self.lineno = lineno
+        self.lexpos = lexpos
+
+class LetBeExpression(VdmslNode):
+    """ letbe式 """
+    _field = ('binding', 'Option', 'body',)
+
+    def __init__(self, binding, option, body, lineno, lexpos):
+        self.binding = binding
+        self.option = option
+        self.body = body
+        self.lineno = lineno
+        self.lexpos = lexpos
+ 
 
 # デバッグ用記述
 if __name__ == '__main__':
